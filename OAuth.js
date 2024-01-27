983c04cb3f9b40bff62cd4a09a8820f89813b84a
Divyanshu <dvishu2610@gmail.com>
2024-01-27 18:43:54 +0530

    Adding Logout for to Delete Refresh Token

--- OAuth.js
+++ OAuth.js
@@ -11,15 +11,21 @@ let refreshTokens = [];
 
 app.post("/token", (req, res) => {
     const refreshToken = req.body.token;
-    if(refreshToken == null) return res.sendStatus(401);
-    if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+    if (refreshToken == null) return res.sendStatus(401);
+    if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
     jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-        if(err) return res.sendStatus(403);
-        const accessToken = generateAccessToken({name: user.name});
+        if (err) return res.sendStatus(403);
+        const accessToken = generateAccessToken({ name: user.name });
         res.json({
-            accessToken : accessToken
+            accessToken: accessToken
         })
     })
+})
+
+app.delete('/logout', (req, res) => {
+    // This filters out the unused tokens i.e. only keep unused tokens
+    refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+    res.sendStatus(204)
 })
 
 
@@ -35,8 +41,8 @@ app.post("/login", (req, res) => {
     })
 })
 
-function generateAccessToken(user){
-    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "20s"});
+function generateAccessToken(user) {
+    return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "20s" });
 }
 
 console.log("Server listening one 8000 :)");
